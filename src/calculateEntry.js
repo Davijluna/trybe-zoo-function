ea761dc15082b1ad50fdb5f314524425fde219d3
Davi Luna <davijluna@gmail.com>
2022-03-16 15:01:36 -0300

    iniciando

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,10 +2,12 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
+
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+
 }
 
 module.exports = { calculateEntry, countEntrants };
