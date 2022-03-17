43552900d24770fa5c22280f5666f9f6322edeba
Davi Luna <davijluna@gmail.com>
2022-03-17 16:37:17 -0300

    exercicio 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
 function isManager(id) {
-  // seu código aqui
+  const gerente = employees.some((lider) => lider.id === id);
+  return gerente;
 }
-
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const funcionários = employees.filter((lider) => lider.managers.includes(managerId));
+    return funcionários.map((pessoa) => `${pessoa.firstName} ${pessoa.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 module.exports = { isManager, getRelatedEmployees };
