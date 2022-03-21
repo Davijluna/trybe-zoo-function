43baa0d1554b6a4faf559fb516f9425d9a032714
Davi Luna <davijluna@gmail.com>
2022-03-20 21:04:26 -0300

    exercicio 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const animals = {};
+    // eslint-disable-next-line array-callback-return
+    species.map((bicho) => { animals[bicho.name] = bicho.residents.length; });
+    return animals;
+  }
+
+  if (Object.keys(animal).length === 1) {
+    const zoo = species.find((elemento) => elemento.name === animal.specie);
+    return zoo.residents.length;
+  }
+
+  if (Object.keys(animal).length === 2) {
+    const zoo = species.find((elemento) => elemento.name === animal.specie).residents;
+    return zoo.filter((elemento2) => elemento2.sex === animal.sex).length;
+  }
 }
 
 module.exports = countAnimals;
