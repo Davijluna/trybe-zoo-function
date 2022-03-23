7a0c740658c524c86b5f1f71ee1fa6a214d7cd2e
Davi Luna <davijluna@gmail.com>
2022-03-22 22:05:46 -0300

    requesito 9

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,13 +1,33 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
+const { prices } = data;
 
+// const entrants = [
+//   { name: 'Lara Carvalho', age: 5 },
+//   { name: 'Frederico Moreira', age: 5 },
+//   { name: 'Pedro Henrique Carvalho', age: 5 },
+//   { name: 'Maria Costa', age: 18 },
+//   { name: 'Núbia Souza', age: 18 },
+//   { name: 'Carlos Nogueira', age: 50 },
+// ];
+
+function countEntrants(entrant) {
+  const childs = entrant.filter((e) => e.age < 18).length;
+  const adults = entrant.filter((e) => e.age >= 18 && e.age < 50).length;
+  const seniors = entrant.filter((e) => e.age >= 50).length;
+  return { child: childs, adult: adults, senior: seniors };
 }
 
-function calculateEntry(entrants) {
-  // seu código aqui
-
+function calculateEntry(entrant) {
+  if (!entrant || Object.keys(entrant).length === 0) {
+    return 0;
+  }
+  const childs = entrant.filter((e) => e.age < 18).length * prices.child;
+  const adults = entrant.filter((e) => e.age >= 18 && e.age < 50).length * prices.adult;
+  const seniors = entrant.filter((e) => e.age >= 50).length * prices.senior;
+  const preco = [childs, adults, seniors];
+  const total = preco.reduce((acc, curr) => acc + curr);
+  return total;
 }
-
+// console.log(calculateEntry([{ name: 'test', age: 18 }]));
 module.exports = { calculateEntry, countEntrants };
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+const { employees, species } = data;
 
+function getOldestFromFirstSpecies(id) {
+  const idFuncionario = employees.find((lider) => lider.id === id).responsibleFor[0];
+  const tipoDeAnimal = species.find((bicho) => bicho.id === idFuncionario).residents;
+  const recebeAnimalAno = tipoDeAnimal.reduce((acc, curr) => Math.max(acc, curr.age), 0);
+  const recebeAnimalVelho = tipoDeAnimal.find((elemento) => elemento.age === recebeAnimalAno);
+  // console.log(recebeAnimalVelho.name);
+  return [recebeAnimalVelho.name, recebeAnimalVelho.sex, recebeAnimalVelho.age];
+}
+// console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
