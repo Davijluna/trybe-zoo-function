79b55c4eb0e156cddbdad637e49480518b5ad375
davi <davijluna@gmail.com>
2023-05-02 14:50:37 -0300

    refactor: adicionando alterações para o push de desvinculação do repositorio da trybe

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -5,10 +5,14 @@ const { employees, species } = data;
 function getOldestFromFirstSpecies(id) {
   const idFuncionario = employees.find((lider) => lider.id === id).responsibleFor[0];
   const tipoDeAnimal = species.find((bicho) => bicho.id === idFuncionario).residents;
+  console.log(species.)
   const recebeAnimalAno = tipoDeAnimal.reduce((acc, curr) => Math.max(acc, curr.age), 0);
   const recebeAnimalVelho = tipoDeAnimal.find((elemento) => elemento.age === recebeAnimalAno);
+  // const AnimalVelho = recebeAnimalVelho.map((chefe) => chefe )
+  // console.log(tipoDeAnimal);
   // console.log(recebeAnimalVelho.name);
   return [recebeAnimalVelho.name, recebeAnimalVelho.sex, recebeAnimalVelho.age];
 }
 // console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
+// getOldestFromFirstSpecies();
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
-
+  const dia = scheduleTarget;
+  return dia;
 }
+console.log(getSchedule(data));
 
 module.exports = getSchedule;
